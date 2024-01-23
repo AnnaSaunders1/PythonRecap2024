86d3f973fadb6f5801a364a126ea0eef18b8573f
Anna Saunders <anna_saunders1@baylor.edu>
2024-01-23 13:02:12 -0600

    updated print statement

--- MyGraph.py
+++ MyGraph.py
@@ -1,3 +1,13 @@
+##remember that python is an open source coding platform
+
+##steps to create a virtual environment
+    ##  1. create the virtual environment
+    ##      py -3 -m venv name (this is the command to create the environment that is put in the terminal)
+    ##  2. Activate the virtual environment
+    ##      .\myvenv\Scripts\activate
+    ##  3. Install 3rd party library/module
+    ##      pip3 install 'module name'
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,4 +16,4 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
